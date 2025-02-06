70f04e1639c479ce66c749e066579cd744602203
PyeonMinjun <alswns9810@gmail.com>
2025-02-06 21:20:54 +0900

    feat : exception 핸들러 작성
    user 및 device 예외 핸들러 작성

---
+++ backend/Split/src/main/java/com/ssafy/Split/bowling/exception/handle/BowlingExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.ssafy.Split.bowling.exception.handle;
+
+import com.ssafy.Split.bowling.exception.DeviceInUseException;
+import com.ssafy.Split.bowling.exception.DeviceNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice("com.ssafy.Split.bowling")
+public class BowlingExceptionHandler {
+
+    @ExceptionHandler(DeviceNotFoundException.class)
+    public ResponseEntity<String> handleDeviceNotFoundException(DeviceNotFoundException e) {
+        log.error("Device not found", e);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(DeviceInUseException.class)
+    public ResponseEntity<String> handleDeviceInUseException(DeviceInUseException e) {
+        log.error("Device in use", e);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+}
---
+++ backend/Split/src/main/java/com/ssafy/Split/exception/handle/DeviceExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.ssafy.Split.exception.handle;
+
+public class DeviceExceptionHandler {
+}
