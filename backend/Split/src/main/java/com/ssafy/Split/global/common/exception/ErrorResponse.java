f02efc91773a9ae65ad47cd1ce42cc3da031e0e0
PyeonMinjun <alswns9810@gmail.com>
2025-02-09 12:24:50 +0900

    fix : 공통 에러 response 수정 및 게임, 랭크 데이터 추가 랭크데이터및 게임 데이터를 추가하고 공통 response에 data 필드 추가

--- backend/Split/src/main/java/com/ssafy/Split/global/common/exception/ErrorResponse.java
+++ backend/Split/src/main/java/com/ssafy/Split/global/common/exception/ErrorResponse.java
@@ -16,12 +16,23 @@ public class ErrorResponse {
     private int status;
     private String message;
     private String timestamp;
+    private Object data;
 
     public static ErrorResponse of(ErrorCode errorCode, String... args) {
         return ErrorResponse.builder()
                 .code(errorCode.getCode())
                 .status(errorCode.getStatus())
                 .message(String.format(errorCode.getMessage(), args))
+                .timestamp(LocalDateTime.now().toString())
+                .build();
+    }
+
+    public static ErrorResponse success(String message, Object data) {
+        return ErrorResponse.builder()
+                .code("SUCCESS")
+                .status(200)
+                .message(message)
+                .data(data)
                 .timestamp(LocalDateTime.now().toString())
                 .build();
     }
