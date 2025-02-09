796cefac63f7b59d2e9c8724f1e4329bd8a51039
PyeonMinjun <alswns9810@gmail.com>
2025-02-09 12:20:53 +0900

    feat : 랭킹조회api 컨트롤러, DTO, 서비스 구현
    랭킹조회를 위한 controller, DTO, service 구현

---
+++ backend/Split/src/main/java/com/ssafy/Split/domain/rank/service/RankService.java
@@ -0,0 +1,38 @@
+package com.ssafy.Split.domain.rank.service;
+
+import com.ssafy.Split.domain.rank.domain.dto.response.RankingResponse;
+import com.ssafy.Split.domain.rank.repository.RankRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RankService {
+
+    private final RankRepository rankRepository;
+
+    public List<RankingResponse.RankData> getRanking() {
+        return rankRepository.findAllOrderByPoseAvgscoreDesc()
+                .stream()
+                .map(rank -> RankingResponse.RankData.builder()
+                        .gameId(rank.getGame().getId())
+                        .userId(rank.getUser().getId())
+                        .nickname(rank.getNickname())
+                        .highlight(rank.getHighlight())
+                        .totalGameCount(rank.getTotalGameCount())
+                        .gameDate(rank.getGameDate().toString())
+                        .poseHighscore(rank.getPoseHighscore())
+                        .poseLosescore(rank.getPoseLosescore())
+                        .poseAvgscore(rank.getPoseAvgscore())
+                        .elbowAngleScore(rank.getElbowAngleScore())
+                        .armStabilityScore(rank.getArmStabilityScore())
+                        .armSpeed(rank.getArmSpeed())
+                        .build())
+                .collect(Collectors.toList());
+    }
+}
