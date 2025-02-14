d14586f50e197c6cedf2f304d3d31cce08d03704
PyeonMinjun <alswns9810@gmail.com>
2025-02-14 10:34:45 +0900

    fix : 하이라이트 s3 검증 로직 추가, jwt필터 추가 userService 수정,삭제 로직 s3에서 해당 영상 없을 때 에러 처리 추가 - jwt 필터에서 헤더로 추가로 보낼시 오류처리 추가 - s3config 환경변수 변경 및 추가

--- backend/Split/src/main/java/com/ssafy/Split/global/filter/JWTFilter.java
+++ backend/Split/src/main/java/com/ssafy/Split/global/filter/JWTFilter.java
@@ -1,16 +1,15 @@
 package com.ssafy.Split.global.filter;
 
-import com.ssafy.Split.global.common.JWT.domain.CustomUserDetails;
 import com.ssafy.Split.domain.user.domain.entity.User;
+import com.ssafy.Split.global.common.JWT.domain.CustomUserDetails;
 import com.ssafy.Split.global.common.JWT.util.JWTUtil;
 import com.ssafy.Split.global.common.exception.ErrorCode;
 import com.ssafy.Split.global.common.exception.SplitException;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,50 +17,50 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 @Slf4j
 @AllArgsConstructor
 public class JWTFilter extends OncePerRequestFilter {
-    private final JWTUtil jwtUtil;
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+  private final JWTUtil jwtUtil;
 
-        //헤더에서 access키에 담긴 토큰을 꺼냄
-        String accessToken = request.getHeader("Authorization");
-        //토큰이 없으면 다음 필터로 넘김 (토큰이 필요없는 요청도 있을 수 있기 때문에)
-        if(accessToken == null){
-            filterChain.doFilter(request,response);
-            return;
-        }
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
 
-        //토큰 만료 여부 확인, 만료시 다음 필터로 안넘김
-        jwtUtil.isExpired(accessToken);
-
-        //토큰이 access인지 확인(발급 시 페이로드에 명시)
-        String category = jwtUtil.getCategory(accessToken);
-
-        if(!category.equals("access")){
-            throw new SplitException(ErrorCode.FORBIDDEN_ACCESS);
-        }
-
-        //일시적인 세션 생성
-        int id = jwtUtil.getId(accessToken);
-        String email = jwtUtil.getEmail(accessToken);
-        String nickname = jwtUtil.getNickname(accessToken);
-
-        User user = User.builder()
-                .id(id)
-                .email(email)
-                .nickname(nickname)
-                .build();
-
-        CustomUserDetails customUserDetails = new CustomUserDetails(user);
-        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null,customUserDetails.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(authToken);
-
-        filterChain.doFilter(request,response);
+    //헤더에서 access키에 담긴 토큰을 꺼냄
+    String accessToken = request.getHeader("Authorization");
+    //토큰이 없으면 다음 필터로 넘김 (토큰이 필요없는 요청도 있을 수 있기 때문에)
+    if (accessToken == null || accessToken.isEmpty()) {
+      filterChain.doFilter(request, response);
+      return;
     }
+
+    //토큰 만료 여부 확인, 만료시 다음 필터로 안넘김
+    jwtUtil.isExpired(accessToken);
+
+    //토큰이 access인지 확인(발급 시 페이로드에 명시)
+    String category = jwtUtil.getCategory(accessToken);
+
+    if (!category.equals("access")) {
+      throw new SplitException(ErrorCode.FORBIDDEN_ACCESS);
+    }
+
+    //일시적인 세션 생성
+    int id = jwtUtil.getId(accessToken);
+    String email = jwtUtil.getEmail(accessToken);
+    String nickname = jwtUtil.getNickname(accessToken);
+
+    User user = User.builder()
+        .id(id)
+        .email(email)
+        .nickname(nickname)
+        .build();
+
+    CustomUserDetails customUserDetails = new CustomUserDetails(user);
+    Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null,
+        customUserDetails.getAuthorities());
+    SecurityContextHolder.getContext().setAuthentication(authToken);
+
+    filterChain.doFilter(request, response);
+  }
 }
--- backend/Split/src/main/java/com/ssafy/Split/global/infra/s3/config/S3Config.java
+++ backend/Split/src/main/java/com/ssafy/Split/global/infra/s3/config/S3Config.java
@@ -12,21 +12,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
+  @Value("${cloud.aws.credentials.access-key}")
+  private String accessKey;
 
-    @Value("${cloud.aws.region.static}")
-    private String region;
+  @Value("${cloud.aws.credentials.secret-key}")
+  private String secretKey;
 
-    @Bean
-    public AmazonS3 amazonS3() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-                .build();
-    }
+  @Value("${cloud.aws.s3.region}")
+  private String region;
+
+  @Bean
+  public AmazonS3 amazonS3() {
+    BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+    return AmazonS3ClientBuilder.standard()
+        .withRegion(region)
+        .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+        .build();
+  }
 }
